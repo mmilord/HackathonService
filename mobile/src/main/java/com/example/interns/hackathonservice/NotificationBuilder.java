56ffbeec51c54918ac833d0879b242a14c51f856
mmilord <mmilord@gmail.com>
2015-06-23 17:45:09 -0400

    Initial commit

---
+++ mobile/src/main/java/com/example/interns/hackathonservice/NotificationBuilder.java
@@ -0,0 +1,16 @@
+package com.example.interns.hackathonservice;
+
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by interns on 6/23/15.
+ */
+public class NotificationBuilder {
+    Context context;
+    Intent intent;
+
+    public NotificationBuilder(Context context, Intent intent) {
+        this.context = context;
+    }
+}
